600c334f2af4647b5e0f4b8870e9973b13dfeaec
Natali-Pchelkina <n.pchelka11@yandex.by>
2021-05-24 23:31:58 +0300

    replaced the variables, deleted empty lines

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ console.log(test); //В консоле значение 2//
 console.log(Boolean(test)); //В консоле значение 'true'//
 
 //Task 9//
-let arr = [2, 3, 5, 8];
+const arr = [2, 3, 5, 8];
 let result = 1;
 for(let i = 0; i<arr.length; i++) {
     result *= arr[i];
@@ -26,7 +26,7 @@ for(let i = 0; i<arr.length; i++) {
 console.log('result', result);
 
 //Task 10
-let arr10 = [2, 5, 8, 15, 0, 6, 20, 3];
+const arr10 = [2, 5, 8, 15, 0, 6, 20, 3];
 for(let i = 0; i < arr10.length; ++i) {
     if(arr10[i] > 5 && arr10[i] < 10) {
     console.log(arr10[i]);
@@ -34,12 +34,9 @@ for(let i = 0; i < arr10.length; ++i) {
 }
 
 //Task 11
-let arr11 = [2, 5, 8, 15, 0, 6, 20, 3];
+const arr11 = [2, 5, 8, 15, 0, 6, 20, 3];
 for(let i = 0; i < arr11.length; ++i) {
     if(!(arr11[i] % 2)) {
         console.log(arr11[i]);
     }
 }
-
- 
-
